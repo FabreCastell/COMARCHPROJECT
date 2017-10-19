e5d9b68ab45c206052441e4df7d1ed3c18a7d9a2
FaberCastell <vorason.si@gmail.com>
2017-10-19 23:56:07 +0700

    start

---
+++ assembler.java
@@ -0,0 +1,25 @@
+// scan and cut file
+
+
+// R-type
+
+
+// I-type 
+
+
+// J-type
+
+
+
+// O-type
+
+
+
+
+ public static void main(String []args) {
+
+      System.out.println("Hello World"); // prints Hello World
+      
+   }
+
+
