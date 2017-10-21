2d75cc64e3033bc0baa9ae5fad8efaa3ce14746e
winner <wachirawit.kan@gmail.com>
2017-10-21 17:46:48 +0700

    compareString

--- Simulator/src/com/company/Main.java
+++ Simulator/src/com/company/Main.java
@@ -6,19 +6,23 @@ public class Main {
 	// write your code here
 
         String s[] = {"8454151","9043971","655361"};
-        String x="";
-        int n[] = new int [3] ;
+        String x[] = new String[s.length];
+        int n[] = new int [s.length] ;
 
         for (int i = 0 ; i < n.length ;i ++){
             n[i]=Integer.parseInt(s[i]);
             System.out.println(n[i]);
-            x=Integer.toBinaryString(n[i]);
 
-            if(24-x.length() != 0 ) x = "0000" + x ;
-            System.out.println(x);
-            System.out.println(x.length() + "\n");
+            x[i]=Integer.toBinaryString(n[i]);
+
+            while(25-x[i].length() != 0 ) x[i] = "0" + x[i] ;
+
+            System.out.print(x[i] + "(");
+            System.out.println(x[i].length()+")" + "\n");
         }
 
-
+        String temp = x[0].substring(0, 3);
+        System.out.println(temp);
+        System.out.println(temp.contains("010"));
     }
 }
